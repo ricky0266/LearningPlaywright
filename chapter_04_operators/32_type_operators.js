7515f1917f7e70c7c860abdd48c648492aeba9c4
Rushil Garg <rickygrg760@gmail.com>
2026-02-23 18:37:12 +0530

    Add chapter 05 - test exercises (41a_test1, 41b_test2)

---
+++ chapter_04_operators/32_type_operators.js
@@ -0,0 +1,12 @@
+console.log(typeof "hello");
+console.log(typeof 123); // int -> number
+console.log(typeof 31.4); // float -> number
+// typeof true
+// typeof undefined  -> undefined
+// typeof null  -> object
+// typeof [] -> object
+console.log(typeof []); // -> object
+
+console.log(`${6 * 4}`);
+
+console.log(typeof null);
