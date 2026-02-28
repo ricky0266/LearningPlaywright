7b61a37eef3c77573f0cc0c0b1da93be65f26a30
Rushil Garg <rickygrg760@gmail.com>
2026-02-28 08:17:00 +0530

    Fix switch statement in statuscode.js and add testresults.js

--- test2/statuscode.js
+++ test2/statuscode.js
@@ -1,39 +1,41 @@
 // As an SDET, you receive an API response status code and need to classify it. Write a JavaScript program using a switch statement that takes an HTTP status code stored in a variable and prints the category and a QA-friendly message.
 
 
-let statusCode = 401;
-if (statusCode = 200) {
-    console.log("PASS - OK: Request successful")
-}
+let statusCode = 200;
+switch (true) {
+    case (statusCode = 200):
+        console.log("PASS - OK: Request successful");
+        break;
 
-else if (statusCode = 201) {
-    console.log("PASS - Created: Resource created successfully")
-}
+    case (statusCode = 201):
+        console.log("PASS - Created: Resource created successfully");
+        break;
 
-else if (statusCode = 301) {
-    console.log("WARNING - Moved Permanently: URL has changed")
-}
+    case (statusCode = 301):
+        console.log("WARNING - Moved Permanently: URL has changed");
+        break;
 
-else if (statusCode = 400) {
-    console.log(FAIL - Bad Request: Check request payload")
-}
+    case (statusCode = 400):
+        console.log("FAIL - Bad Request: Check request payload");
+        break;
 
-else if (statusCode = 401) {
-    console.log("FAIL - Unauthorized: Check auth token")
-}
+    case (statusCode = 401):
+        console.log("FAIL - Unauthorized: Check auth token");
+        break;
 
-else if (statusCode = 403) {
-    console.log("FAIL - Forbidden: Insufficient permissions")
-}
+    case (statusCode = 403):
+        console.log("FAIL - Forbidden: Insufficient permissions");
+        break;
 
-else if (statusCode = 404) {
-    console.log("FAIL - Not Found: Check endpoint URL")
-}
+    case (statusCode = 404):
+        console.log("FAIL - Not Found: Check endpoint URL");
+        break;
 
-else if (statusCode = 500) {
-    console.log("FAIL - Internal Server Error: Backend issue")
-}
+    case (statusCode = 500):
+        console.log("FAIL - Internal Server Error: Backend issue");
+        break;
 
-else {
-    console.log("UNKNOWN - Unhandled status code")
+    default:
+
+        console.log("UNKNOWN - Unhandled status code");
 }
